a017e344f253399458157f5571f2dc5a2fd89184
duzeyu <vae.dzy@gmail.com>
2017-12-18 20:18:42 +0800

    reboot

--- src/com/account/domain/User.java
+++ src/com/account/domain/User.java
@@ -2,21 +2,31 @@ package com.account.domain;
 
 public class User {
     private Integer id;
-    private String name;
+    private String phone;
+    private String username;
 
     public Integer getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setId(Integer id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
+
--- src/com/account/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.account.service;
-
-import com.account.domain.User;
-
-public interface UserService {
-    public User getUserById(int id);
-    public User findUserByName(String username);
-}
